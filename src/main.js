848c6cf89db28b56eefe990a64ab1650f8769066
James nolan <jamesnolan1@hotmail.com>
2021-12-08 10:57:08 +0000

    Final responsive mobile adjustments

--- src/main.js
+++ src/main.js
@@ -1,19 +1,40 @@
-
-
-$(document).ready(function(){
-    $('.product-carousel').slick({
-        infinite: false,
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        appendArrows: $('.product-carousel'),
-        dots: true,
-    });
+$(document).ready(function () {
+  $(".product-carousel").slick({
+    infinite: false,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    appendArrows: $(".product-carousel"),
+    dots: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
   });
+});
 
-  const accordion = document.getElementsByClassName('container');
+const accordion = document.getElementsByClassName("container");
 
-  for (i=0; i<accordion.length; i++) {
-    accordion[i].addEventListener('click', function () {
-      this.classList.toggle('active')
-    })
-  };
+for (i = 0; i < accordion.length; i++) {
+  accordion[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+  });
+}
