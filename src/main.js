8b333e8e81c45f2f5a0ee2bf7f177b5a7ac2fc20
James nolan <jamesnolan1@hotmail.com>
2021-12-09 00:09:38 +0000

    JS Col height setter

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
 $(document).ready(function () {
+  $(".row-eq").each(function () {
+    equalColHeights($(this));
+  });
   $(".product-carousel").slick({
     infinite: false,
     slidesToShow: 3,
@@ -31,10 +34,29 @@ $(document).ready(function () {
   });
 });
 
+window.onresize = function () {
+  $(".row-eq").each(function () {
+    equalColHeights($(this));
+  });
+};
+
 const accordion = document.getElementsByClassName("container");
 
 for (i = 0; i < accordion.length; i++) {
   accordion[i].addEventListener("click", function () {
     this.classList.toggle("active");
   });
+}
+
+function equalColHeights(ele) {
+  var highestCol = 0;
+  $(ele)
+    .children(".col")
+    .each(function () {
+      $(this).css("height", "auto");
+      if (highestCol < $(this).height()) {
+        highestCol = $(this).height();
+      }
+      $(ele).children(".col").height(highestCol);
+    });
 }
